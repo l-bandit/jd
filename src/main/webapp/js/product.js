a5d10fb5acc5069f77b49899204f76b9ee38ae54
zhangjuhan <627583600@qq.com>
2019-08-03 16:08:59 +0800

    张鞠涵的前端页面

---
+++ src/main/webapp/js/product.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	$("#mid_purchase").click(function(){
+		$("#slide").slideToggle("slow");
+		$("#mid").css("filter","brightness(0.6)");
+	})	
+	$("#bottom_right").click(function(){
+		$("#slide").slideToggle("slow");
+		$("#mid").css("filter","brightness(0.6)");
+	})
+	$("#slide_close").click(function(){
+		$("#slide").slideToggle("slow");
+		$("#mid").css("filter","none");
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+})
